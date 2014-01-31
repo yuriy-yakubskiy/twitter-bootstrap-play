12ae5dc0d3f7ba955e5b576288e6788ab50e4639
Yuriy <yakubskiy2002@gmail.com>
2014-01-31 19:46:16 +0200

    initial commit 2

---
+++ body.php
@@ -0,0 +1,120 @@
+<div class=".container-fluid">
+    <div class="col-md-2 hidden-sm hidden-xs">
+        <div id="sidebar">
+            <div class="sidebar-nav">
+                <ul class="nav">
+                    <li class="first">        <span class="nav-header">                Assortment        </span>            <ul class="nav menu_level_1">
+                            <li class="first">        <a href="/sylius/web/app_dev.php/administration/taxonomies/">    <i class="glyphicon glyphicon-folder-close"></i>            Categorization        </a>        
+                            </li>
+
+                            <li>        <a href="/sylius/web/app_dev.php/administration/products/">    <i class="glyphicon glyphicon-th-list"></i>            Products        </a>        
+                            </li>
+
+                            <li>        <a href="/sylius/web/app_dev.php/administration/inventory/">    <i class="glyphicon glyphicon-tasks"></i>            Inventory levels        </a>        
+                            </li>
+
+                            <li>        <a href="/sylius/web/app_dev.php/administration/options/">    <i class="glyphicon glyphicon-th"></i>            Manage options        </a>        
+                            </li>
+
+                            <li>        <a href="/sylius/web/app_dev.php/administration/properties/">    <i class="glyphicon glyphicon-list-alt"></i>            Configure properties        </a>        
+                            </li>
+
+                            <li class="last">        <a href="/sylius/web/app_dev.php/administration/prototypes/">    <i class="glyphicon glyphicon-compressed"></i>            Product prototypes        </a>        
+                            </li>
+
+
+                        </ul>
+
+                    </li>
+
+                    <li>        <span class="nav-header">                Orders        </span>            <ul class="nav menu_level_1">
+                            <li class="first">        <a href="/sylius/web/app_dev.php/administration/orders/">    <i class="glyphicon glyphicon-shopping-cart"></i>            Current orders        </a>        
+                            </li>
+
+                            <li>        <a href="/sylius/web/app_dev.php/administration/shipments/">    <i class="glyphicon glyphicon-plane"></i>            Shipments        </a>        
+                            </li>
+
+                            <li>        <a href="/sylius/web/app_dev.php/administration/payments/">    <i class="glyphicon glyphicon-credit-card"></i>            Payments        </a>        
+                            </li>
+
+                            <li>        <a href="/sylius/web/app_dev.php/administration/promotions/">    <i class="glyphicon glyphicon-bullhorn"></i>            Promotions        </a>        
+                            </li>
+
+                            <li class="last">        <a href="/sylius/web/app_dev.php/administration/promotions/new">    <i class="glyphicon glyphicon-plus-sign"></i>            Add new promotion        </a>        
+                            </li>
+
+
+                        </ul>
+
+                    </li>
+
+                    <li>        <span class="nav-header">                Customers        </span>            <ul class="nav menu_level_1">
+                            <li class="first">        <a href="/sylius/web/app_dev.php/administration/users/">    <i class="glyphicon glyphicon-user"></i>            Users        </a>        
+                            </li>
+
+                            <li class="last">        <a href="/sylius/web/app_dev.php/administration/groups/">    <i class="glyphicon glyphicon-home"></i>            Groups        </a>        
+                            </li>
+
+
+                        </ul>
+
+                    </li>
+
+                    <li>        <span class="nav-header">                Content        </span>            <ul class="nav menu_level_1">
+                            <li class="first">        <a href="/sylius/web/app_dev.php/administration/blocks/">    <i class="glyphicon glyphicon-th-large"></i>            Blocks        </a>        
+                            </li>
+
+                            <li class="last">        <a href="/sylius/web/app_dev.php/administration/pages/">    <i class="glyphicon glyphicon-th-list"></i>            Pages        </a>        
+                            </li>
+
+
+                        </ul>
+
+                    </li>
+
+                    <li class="last">        <span class="nav-header">                Configuration        </span>            <ul class="nav menu_level_1">
+                            <li class="first">        <a href="/sylius/web/app_dev.php/administration/settings/general">    <i class="glyphicon glyphicon-info-sign"></i>            General settings        </a>        
+                            </li>
+
+                            <li>        <a href="/sylius/web/app_dev.php/administration/locales/">    <i class="glyphicon glyphicon-flag"></i>            Locales        </a>        
+                            </li>
+
+                            <li>        <a href="/sylius/web/app_dev.php/administration/payment-methods/">    <i class="glyphicon glyphicon-credit-card"></i>            Payment methods        </a>        
+                            </li>
+
+                            <li>        <a href="/sylius/web/app_dev.php/administration/exchange-rates/">    <i class="glyphicon glyphicon-usd"></i>            Exchange rates        </a>        
+                            </li>
+
+                            <li>        <a href="/sylius/web/app_dev.php/administration/settings/taxation">    <i class="glyphicon glyphicon-cog"></i>            Taxation settings        </a>        
+                            </li>
+
+                            <li>        <a href="/sylius/web/app_dev.php/administration/tax-categories/">    <i class="glyphicon glyphicon-cog"></i>            Taxation categories        </a>        
+                            </li>
+
+                            <li>        <a href="/sylius/web/app_dev.php/administration/tax-rates/">    <i class="glyphicon glyphicon-cog"></i>            Tax rates        </a>        
+                            </li>
+
+                            <li>        <a href="/sylius/web/app_dev.php/administration/shipping-categories/">    <i class="glyphicon glyphicon-cog"></i>            Shipping categories        </a>        
+                            </li>
+
+                            <li>        <a href="/sylius/web/app_dev.php/administration/shipping-methods/">    <i class="glyphicon glyphicon-cog"></i>            Shipping methods        </a>        
+                            </li>
+
+                            <li>        <a href="/sylius/web/app_dev.php/administration/countries/">    <i class="glyphicon glyphicon-flag"></i>            Countries        </a>        
+                            </li>
+
+                            <li class="last">        <a href="/sylius/web/app_dev.php/administration/zones/">    <i class="glyphicon glyphicon-globe"></i>            Zones        </a>        
+                            </li>
+
+
+                        </ul>
+
+                    </li>
+
+
+                </ul>
+
+            </div>
+        </div>
+    </div>   
+</div>
